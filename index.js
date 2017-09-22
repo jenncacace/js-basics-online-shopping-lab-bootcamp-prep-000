8b4cceb6a132a4becf89b63840a1d0602287d2f2
Jenn Cacace <jenn.cacace@gmail.com>
2017-09-22 00:36:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ let isItemInCart = false
   for (var i = 0; i < cart.length; i++){
     if (cart[i].hasOwnProperty(item) === true) {
       cart = cart.splice(0, i) + cart.slice (i+1)
-    } 
+    }
   } else {
     console.log("The item is not in your cart.")
   }
