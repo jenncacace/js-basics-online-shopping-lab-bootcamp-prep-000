0e5f55c610fa905416d42a4ad264cc648360dba3
Jenn Cacace <jenn.cacace@gmail.com>
2017-09-22 00:59:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -68,12 +68,12 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   if (!cardNumber){
     console.log("Sorry, we don't have a credit card on file for you.")
-    return cart
-  } else {
+    return false
+  } 
     console.log (`Your total is ${total()}, which will be charged to the card ${cardNumber}.`)
   }
   cart = []
-  return cart
+
 }
 
 //addToCart("banana")
