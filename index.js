7f467e8901b054d85789117d3ca6a5fe8bb31938
Jenn Cacace <jenn.cacace@gmail.com>
2017-09-22 01:02:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -66,14 +66,14 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (!cardNumber){
-    console.log("Sorry, we don't have a credit card on file for you.")
-    return false
+  if (!cardNumber) {
+    console.log("Sorry, we don't have a credit card on file for you.");
+    return false;
   }
-    console.log (`Your total is ${total()}, which will be charged to the card ${cardNumber}.`)
 
-  cart = []
+  console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
 
+  cart = [];
 }
 
 //addToCart("banana")
