f4038b9313815a33d7327c4841782a05226193d5
Jenn Cacace <jenn.cacace@gmail.com>
2017-09-22 00:59:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ function placeOrder(cardNumber) {
   if (!cardNumber){
     console.log("Sorry, we don't have a credit card on file for you.")
     return false
-  } 
+  }
     console.log (`Your total is ${total()}, which will be charged to the card ${cardNumber}.`)
   }
   cart = []
